fd4b1bbe96db20467acc0dc8db47f7760c524445
mqiqe <mqiqe@163.com>
2018-07-20 09:52:57 +0800

    提交最新

---
+++ src/main/java/com/mqiqe/WfgServiceHiApplication.java
@@ -0,0 +1,12 @@
+package com.mqiqe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfgServiceHiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WfgServiceHiApplication.class, args);
+	}
+}
